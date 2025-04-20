d934de0dea773d97bf0526ad5e78ef4d70db2e89
dimeskigj <dimeskigj@gmail.com>
2025-04-20 11:36:28 +0200

    Implement UI

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'connect-four';
+  board: number[][] = Array(6)
+    .fill(undefined)
+    .map(() => Array(7).fill(0));
+
+  populateRandomField() {
+    this.board[this.getRandomInt(this.board.length)][
+      this.getRandomInt(this.board[0].length)
+    ] = this.getRandomInt(3);
+  }
+
+  getRandomInt(max: number): number {
+    return Math.floor(Math.random() * max);
+  }
 }
