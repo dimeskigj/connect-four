1414e88b43610ac6c0a850e37883788ba72d0b1d
dimeskigj <dimeskigj@gmail.com>
2025-04-21 18:12:14 +0200

    Implement game

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { GameService } from './services/game.service';
 
 @Component({
   selector: 'app-root',
@@ -10,14 +11,17 @@ export class AppComponent {
   board: number[][] = Array(6)
     .fill(undefined)
     .map(() => Array(7).fill(0));
+  currentPlayer: number = 1;
 
-  populateRandomField() {
-    this.board[this.getRandomInt(this.board.length)][
-      this.getRandomInt(this.board[0].length)
-    ] = this.getRandomInt(3);
+  constructor(private gameService: GameService) {
+    this.gameService.$gameState.subscribe((state) => {
+      this.board = state?.G.board!;
+      this.currentPlayer = +state?.ctx.currentPlayer! + 1;
+      console.log(this.currentPlayer)
+    });
   }
 
-  getRandomInt(max: number): number {
-    return Math.floor(Math.random() * max);
+  dropChecker(column: number): void {
+    this.gameService.dropChecker(column);
   }
 }
---
+++ src/game/connect-four.ts
@@ -0,0 +1,77 @@
+import type { Game, Move } from 'boardgame.io';
+import { INVALID_MOVE } from 'boardgame.io/core';
+
+export interface ConnectFourState {
+  board: number[][];
+}
+
+const dropChecker: Move<ConnectFourState> = ({ G, ctx }, col: number) => {
+  const hasFreeSpace = G.board[0][col] === 0;
+  if (!hasFreeSpace) {
+    return INVALID_MOVE;
+  }
+
+  for (let row = G.board.length - 1; row >= 0; row--) {
+    const isCellEmpty = G.board[row][col] === 0;
+    if (isCellEmpty) {
+      G.board[row][col] = (ctx.turn % 2) + 1;
+      break;
+    }
+  }
+
+  return;
+};
+
+export const ConnectFourGame: Game<ConnectFourState> = {
+  name: 'connect-four',
+  maxPlayers: 2,
+  setup: () => ({
+    board: Array(6)
+      .fill(undefined)
+      .map(() => Array(7).fill(0)),
+  }),
+  turn: {
+    minMoves: 1,
+    maxMoves: 1,
+  },
+  moves: {
+    dropChecker: dropChecker,
+  },
+  endIf: ({ G, ctx }): void | { winner: number } => {
+    const rows = G.board.length;
+    const cols = G.board[0].length;
+
+    const directions = [
+      [0, 1],
+      [1, 0],
+      [1, 1],
+      [1, -1],
+    ];
+
+    for (let row = 0; row < rows; row++) {
+      for (let col = 0; col < cols; col++) {
+        const cell = G.board[row][col];
+        if (cell === 0) continue;
+
+        for (const [dr, dc] of directions) {
+          let count = 1;
+          let r = row + dr;
+          let c = col + dc;
+
+          while (
+            r >= 0 &&
+            r < rows &&
+            c >= 0 &&
+            c < cols &&
+            G.board[r][c] === cell
+          ) {
+            count++;
+            if (count === 4) return { winner: cell };
+            r += dr;
+            c += dc;
+          }
+        }
+      }
+    }
+  },
+};
